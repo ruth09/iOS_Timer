5b8bcbc5357e4394d82808848fb094af0e49511d
Ruthran <ruth09@users.noreply.github.com>
2018-02-11 00:56:21 +0530

    11feb

--- Timer/ViewController.swift
+++ Timer/ViewController.swift
@@ -10,16 +10,75 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var startButton: UIButton!
+
+    @IBOutlet weak var pauseButton: UIButton!
+    
+    @IBOutlet weak var resetButton: UIButton!
+    
+    @IBOutlet weak var labelCounter: UILabel!
+    
+    var counter : Double = 0
+    var timer = Timer()
+    var isrunning = false
+    
+    override var preferredStatusBarStyle: UIStatusBarStyle{
+        return .lightContent
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        startButton.applyDesgin()
+        pauseButton.applyDesgin()
+        resetButton.applyDesgin()
+        labelCounter.text = "\(counter)"
+        startButton.isEnabled = true
+        pauseButton.isEnabled = false
+        resetButton.isEnabled = false
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    @IBAction func start(_ sender: Any) {
+        if !isrunning {
+            timer = Timer.scheduledTimer(timeInterval: 0.1, target: self, selector: #selector(ViewController.updateTimer), userInfo: nil, repeats: true)
+            startButton.isEnabled = false
+            pauseButton.isEnabled = true
+            resetButton.isEnabled = true
+            isrunning = true
+        }
+        
     }
-
-
+    
+    @IBAction func pause(_ sender: Any) {
+        timer.invalidate()
+        startButton.isEnabled = true
+        pauseButton.isEnabled = false
+        isrunning = false
+        
+    }
+    
+    @IBAction func reset(_ sender: Any) {
+        timer.invalidate()
+        counter = 0
+        labelCounter.text = "\(counter)"
+        startButton.isEnabled = true
+        pauseButton.isEnabled = false
+        isrunning = false
+        
+    }
+    
+    @objc func updateTimer(){
+        counter += 0.1
+        labelCounter.text = String(format: "%.1f", counter)
+        
+        
+    }
 }
-
+extension UIButton {
+    func applyDesgin()   {
+        //self.layer.shadowColor = UIColor.darkGray.cgColor
+        //self.layer.shadowOpacity = 0.5
+        //self.layer.shadowRadius = 1
+        self.layer.cornerRadius = self.frame.width / 2
+    }
+    
+}
